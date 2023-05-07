d8f9fd6990824c572cba3bfc1caf8552de25bc22
Maarij <maarij.anwar@gmail.com>
2023-05-07 18:43:52 -0500

    Merge Strings Alternately

---
+++ src/main/java/LeetCodeLauncher.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import main.java.service.MergeStringsAlternately1768;
+
+public class LeetCodeLauncher {
+    public static void main(String[] args) {
+        MergeStringsAlternately1768 service = new MergeStringsAlternately1768();
+
+        System.out.println(service.mergeAlternately("ab", "pqrs"));
+    }
+}
