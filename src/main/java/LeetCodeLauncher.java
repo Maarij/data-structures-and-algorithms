00204ed7af1c3b3d8c9a301f1b55223548b579e1
Maarij <maarij.anwar@gmail.com>
2023-07-19 19:17:04 -0500

    ContainsDuplicate217

--- src/main/java/LeetCodeLauncher.java
+++ src/main/java/LeetCodeLauncher.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import main.java.service.array.ContainsDuplicate217;
 import main.java.service.array.RotateArray189;
 
 public class LeetCodeLauncher {
@@ -13,7 +14,10 @@ public class LeetCodeLauncher {
 //        System.out.println(service.maxProfit(new int[]{1, 2, 3, 4, 5}));
 //        System.out.println(service.maxProfit(new int[]{7, 6, 4, 3, 1}));
 
-        RotateArray189 service = new RotateArray189();
-        service.rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+//        RotateArray189 service = new RotateArray189();
+//        service.rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+
+        ContainsDuplicate217 service = new ContainsDuplicate217();
+        System.out.println(service.containsDuplicate(new int[]{1, 2, 3, 4}));
     }
 }
---
+++ src/main/java/service/array/ContainsDuplicate217.java
@@ -0,0 +1,25 @@
+package main.java.service.array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate217 {
+
+    // Input: nums = [1,2,3,1]
+    //                X     X
+    // true
+    // TC: O(n)
+    // SC: O(n)
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> numsChecked = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (numsChecked.contains(nums[i])) {
+                return true;
+            }
+            numsChecked.add(nums[i]);
+        }
+
+        return false;
+    }
+}
