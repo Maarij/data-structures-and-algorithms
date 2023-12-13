9d09a64e49d22c3d1baf3b7f3ca0f9071c38c091
Maarij <maarij.anwar@gmail.com>
2023-12-12 20:36:35 -0600

    ReverseInteger7

---
+++ src/main/java/service/string/ReverseInteger7.java
@@ -0,0 +1,29 @@
+package main.java.service.string;
+
+public class ReverseInteger7 {
+
+    public static void main(String[] args) {
+        ReverseInteger7 reverseInteger7 = new ReverseInteger7();
+        System.out.println(reverseInteger7.reverse(123));
+    }
+
+    // TC: O(log(n))
+    // SC: O(1)
+    public int reverse(int x) {
+        int result = 0;
+
+        while (x != 0) {
+            int remainder = x % 10;
+            int temp = (result * 10) + remainder;
+
+            if ((temp - remainder) / 10 != result) {
+                return 0;
+            }
+
+            x = x / 10;
+            result = temp;
+        }
+
+        return result;
+    }
+}
