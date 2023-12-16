3112bc18e2e8f510e5c38ae531b5b6bdb57c28a2
Maarij <maarij.anwar@gmail.com>
2023-12-15 18:40:59 -0600

    ImplementStr28

---
+++ src/main/java/service/string/ImplementStr28.java
@@ -0,0 +1,31 @@
+package main.java.service.string;
+
+public class ImplementStr28 {
+
+    public static void main (String[] args) {
+        ImplementStr28 implementStr28 = new ImplementStr28();
+
+        System.out.println(implementStr28.strStr("sadbutsad", "sad"));
+    }
+
+    private int strStr(String haystack, String needle) {
+        int startingIndex = -1;
+        int matchingChars = 0;
+
+        for(int i = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(matchingChars)) {
+                startingIndex = i;
+                matchingChars++;
+            } else {
+                startingIndex = -1;
+                matchingChars = 0;
+            }
+
+            if (matchingChars == needle.length()) {
+                return startingIndex - matchingChars + 1;
+            }
+        }
+
+        return startingIndex;
+    }
+}
