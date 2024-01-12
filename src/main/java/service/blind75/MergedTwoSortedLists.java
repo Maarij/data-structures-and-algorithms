9b34ad8489c2472ff505c5efcf129e09910d6e6f
Maarij <maarij.anwar@gmail.com>
2024-01-11 21:22:58 -0600

    MergedTwoSortedLists

---
+++ src/main/java/service/blind75/MergedTwoSortedLists.java
@@ -0,0 +1,77 @@
+package main.java.service.blind75;
+
+public class MergedTwoSortedLists {
+
+    public static void main(String[] args) {
+        ListNode list1A = new ListNode(1);
+        ListNode list1B = new ListNode(2);
+        ListNode list1C = new ListNode(4);
+        list1A.next = list1B;
+        list1B.next = list1C;
+
+        ListNode list2A = new ListNode(1);
+        ListNode list2B = new ListNode(3);
+        ListNode list2C = new ListNode(4);
+        list2A.next = list2B;
+        list2B.next = list2C;
+
+        ListNode head;
+//        head = mergeListsIterative(list1A, list2A);
+        head = mergeListsRecursive(list1A, list2A);
+
+        return;
+    }
+
+    private static ListNode mergeListsRecursive(ListNode list1, ListNode list2) {
+        if (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                list1.next = mergeListsRecursive(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeListsRecursive(list1, list2.next);
+                return list2;
+            }
+        }
+        if (list1 == null) {
+            return list2;
+        }
+        return list1;
+    }
+
+    static ListNode mergeListsIterative(ListNode list1, ListNode list2) {
+        ListNode prehead = new ListNode(-1);
+        ListNode current = prehead;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        current.next = list1 == null ? list2 : list1;
+
+        return prehead.next;
+    }
+
+    private static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
