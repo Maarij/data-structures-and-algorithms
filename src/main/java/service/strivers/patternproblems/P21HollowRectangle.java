3e615c550866f966323173cd346df220aad0e1aa
Maarij <maarij.anwar@gmail.com>
2024-01-14 15:17:59 -0600

    P22Number

--- src/main/java/service/strivers/patternproblems/P21HollowRectangle.java
+++ src/main/java/service/strivers/patternproblems/P21HollowRectangle.java
@@ -8,14 +8,14 @@ public class P21HollowRectangle {
     //*    *
     //*    *
     //******
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int N = 5;
 
         for (int i = 0; i < N; i++) {
             System.out.print("*");
 
             String hollowPattern = i == 0 || i == N - 1 ? "*" : " ";
-            for (int j = 0; j < N-2; j++) {
+            for (int j = 0; j < N - 2; j++) {
                 System.out.print(hollowPattern);
             }
 
