a83bf872aeaa23c7bbee6ac7a014044249cd3dce
Maarij <maarij.anwar@gmail.com>
2024-01-15 15:39:56 -0600

    P1InvertBinaryTree

---
+++ src/main/java/grind75/week2/P1InvertBinaryTree.java
@@ -0,0 +1,51 @@
+package main.java.grind75.week2;
+
+public class P1InvertBinaryTree {
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+
+        node1.left = node2;
+        node1.right = node3;
+
+        invertTree(node1);
+
+        System.out.println("DONE");
+    }
+
+    static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
