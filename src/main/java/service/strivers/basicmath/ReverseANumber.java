93a3c838acc6877bfe9e2490faf199635436e0b3
Maarij <maarij.anwar@gmail.com>
2024-01-15 08:33:23 -0600

    ReverseANumber

--- src/main/java/service/strivers/basicmath/ReverseANumber.java
+++
@@ -1,7 +0,0 @@
-package main.java.service.strivers.basicmath;
-
-public class ReverseANumber {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/strivers/basicmath/ReverseANumber.java
@@ -0,0 +1,28 @@
+package main.java.strivers.basicmath;
+
+public class ReverseANumber {
+    public static void main(String[] args) {
+        System.out.println(reverse(123));
+    }
+
+    public static int reverse(int x) {
+        int result = 0;
+
+        while (x != 0) {
+            int lastDigit = x % 10;
+
+            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && lastDigit > 7)) {
+                return 0;
+            }
+
+            if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE / 10 && lastDigit < -8)) {
+                return 0;
+            }
+
+            result = result * 10 + lastDigit;
+            x = x / 10;
+        }
+
+        return result;
+    }
+}
