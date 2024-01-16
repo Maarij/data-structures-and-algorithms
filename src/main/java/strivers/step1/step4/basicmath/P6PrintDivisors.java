27c55d5752a17f4ed246dbc90a3d747ac3544693
Maarij <maarij.anwar@gmail.com>
2024-01-16 14:17:10 -0600

    P6PrintDivisors

---
+++ src/main/java/strivers/step1/step4/basicmath/P6PrintDivisors.java
@@ -0,0 +1,24 @@
+package main.java.strivers.step1.step4.basicmath;
+
+public class P6PrintDivisors {
+    public static void main(String[] args) {
+        printDivisors(36);
+    }
+
+    private static void printDivisors(int n) {
+        for (int i = 1; i <= (int) Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+                if (i != n / i) System.out.print(n / i + " ");
+            }
+        }
+    }
+
+    private static void printDivisorsBruteForce(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
