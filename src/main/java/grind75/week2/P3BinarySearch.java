24807e0138bceecc6de285953bacc96849602c49
Maarij <maarij.anwar@gmail.com>
2024-01-18 12:52:35 -0600

    P3BinarySearch

---
+++ src/main/java/grind75/week2/P3BinarySearch.java
@@ -0,0 +1,26 @@
+package main.java.grind75.week2;
+
+public class P3BinarySearch {
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, 9));
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, 12));
+        System.out.println(search(new int[]{-1, 0, 3, 5, 9, 12}, -5));
+    }
+
+    private static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
