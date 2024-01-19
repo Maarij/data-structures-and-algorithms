0039dcfd1123bb32eb648fb6043ee3076be579c9
Maarij <maarij.anwar@gmail.com>
2024-01-19 07:11:38 -0600

    P2PrintNameNTimes

---
+++ src/main/java/strivers/step2/P2PrintNameNTimes.java
@@ -0,0 +1,19 @@
+package main.java.strivers.step2;
+
+public class P2PrintNameNTimes {
+    public static void main(String[] args) {
+        printName(5);
+    }
+
+    private static void printName(int n) {
+        printNameRecursively(1, n);
+    }
+
+    private static void printNameRecursively(int i, int n) {
+        if (i > n) {
+            return;
+        }
+        System.out.println("Maarij");
+        printNameRecursively(i + 1, n);
+    }
+}
