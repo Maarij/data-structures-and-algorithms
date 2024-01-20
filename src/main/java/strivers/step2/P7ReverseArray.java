8536b33b91a52b683889234ee544a3a3f5567327
Maarij <maarij.anwar@gmail.com>
2024-01-20 14:00:26 -0600

    P7ReverseArray

---
+++ src/main/java/strivers/step2/P7ReverseArray.java
@@ -0,0 +1,21 @@
+package main.java.strivers.step2;
+
+public class P7ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = new int[]{5, 4, 3, 2, 1};
+
+        reverseArray(arr, 0, arr.length - 1);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private static void reverseArray(int[] arr, int start, int end) {
+        if (start < end) {
+            int tmp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = tmp;
+            reverseArray(arr, start + 1, end - 1);
+        }
+    }
+}
