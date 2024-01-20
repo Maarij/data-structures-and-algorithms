480ef6bcb98ef053ec115c53bf26b7011cfb4f20
Maarij <maarij.anwar@gmail.com>
2024-01-20 16:09:53 -0600

    P8MockTestFindMedian

---
+++ src/main/java/hackerrank/threemonth/week1/P8MockTestFindMedian.java
@@ -0,0 +1,17 @@
+package main.java.hackerrank.threemonth.week1;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class P8MockTestFindMedian {
+    public static void main(String[] args){
+        System.out.println(findMedian(Arrays.asList(1,6,3,2,1)));
+    }
+
+    public static int findMedian(List<Integer> arr) {
+        Collections.sort(arr, Comparator.comparingInt(o -> o));
+        return arr.get(arr.size()/2);
+    }
+}
