6a5d41d3bc7ba8017d34d7b7ef0f73dfa7a38dca
Maarij <maarij.anwar@gmail.com>
2024-02-06 08:25:25 -0600

    P4MajorityElement

---
+++ src/main/java/grind75/week4/P4MajorityElement.java
@@ -0,0 +1,28 @@
+package main.java.grind75.week4;
+
+public class P4MajorityElement {
+    public static void main(String[] args) {
+        System.out.println(majorityElement(new int[]{3, 2, 3}));
+        System.out.println(majorityElement(new int[]{2, 2, 1, 1, 1, 2, 2}));
+    }
+
+    // 3,2,3
+    // 2,3,3
+
+    // 2,2,2,1,1,2
+    // 1,1,2,2,2,2
+    private static int majorityElement(int[] nums) {
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            count += (candidate == num) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
