6905a27e698e147b2db6951a5f4f68a60952a94c
Maarij <maarij.anwar@gmail.com>
2024-02-17 16:28:45 -0600

    P5MaximumSubarray

---
+++ src/main/java/grind75/week6/P5MaximumSubarray.java
@@ -0,0 +1,19 @@
+package main.java.grind75.week6;
+
+public class P5MaximumSubarray {
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+
+    private static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
