f36302dd213615cc6adfe0d83d8668444b5f1262
Maarij <maarij.anwar@gmail.com>
2024-02-17 19:49:01 -0600

    P8DrawingBook

--- src/main/java/hackerrank/threemonth/week3/P8DrawingBook.java
+++ src/main/java/hackerrank/threemonth/week3/P8DrawingBook.java
@@ -5,13 +5,6 @@ public class P8DrawingBook {
         System.out.println(pageCount(5, 3));
     }
 
-    // - | 1
-    // 2 | 3
-    // 4 | 5
-    // 6 | 7
-    // 8 | -
-
-    // 8 - 2x = 3
     private static int pageCount(int n, int p) {
         int pagesInTheBook = n;
         int pageToReach = p;
