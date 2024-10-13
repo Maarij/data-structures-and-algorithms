10a5bf93bd1013a6f64fb59aed07285f90476e24
Maarij <maarij.anwar@gmail.com>
2024-10-13 11:08:47 -0500

    MergeSortedArray88 - two pointer

---
+++ src/main/java/leetCode/topInterview150/MergeSortedArray88.java
@@ -0,0 +1,34 @@
+package main.java.leetCode.topInterview150;
+
+// https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+public class MergeSortedArray88 {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int[] nums2 = new int[]{2, 5, 6};
+
+        merge(nums1, 3, nums2, 3);
+
+        for (int i : nums1) {
+            System.out.print(i + " ");
+        }
+    }
+
+    // two pointer approach
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                k--;
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+            }
+        }
+    }
+}
