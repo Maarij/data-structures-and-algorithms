d2d367cc0f002f2b941a4ae97ae5fb769d376267
Maarij <maarij.anwar@gmail.com>
2024-12-09 20:05:48 -0600

    206. Reverse Linked List

---
+++ src/main/java/blind75/linkedList/ReverseLinkedList.java
@@ -0,0 +1,60 @@
+package main.java.blind75.linkedList;
+
+/**
+ * <a href="https://leetcode.com/problems/reverse-linked-list/description/">206. Reverse Linked List</a>
+ * <br>
+ * <br>
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ */
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        ListNode listNode = reverse(node1);
+
+        while (listNode != null) {
+            System.out.print(listNode.val + "->");
+            listNode = listNode.next;
+        }
+    }
+
+    private static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
