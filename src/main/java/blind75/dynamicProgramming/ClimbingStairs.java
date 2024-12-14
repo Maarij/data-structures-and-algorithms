3c99fa6367ea04070520c07d3f6cbfd67247d628
Maarij <maarij.anwar@gmail.com>
2024-12-14 16:17:22 -0600

    70. Climbing Stairs

--- src/main/java/blind75/dynamicProgramming/ClimbingStairs.java
+++ src/main/java/blind75/dynamicProgramming/ClimbingStairs.java
@@ -8,7 +8,7 @@ package main.java.blind75.dynamicProgramming;
  * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
  */
 public class ClimbingStairs {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(climbStairs(2));
         System.out.println(climbStairs(3));
         System.out.println(climbStairs(4));
@@ -32,5 +32,4 @@ public class ClimbingStairs {
 
         return ways[n];
     }
-
 }
