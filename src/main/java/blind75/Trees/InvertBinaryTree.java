42a148f4bcbb8c3c4d70c1f198d6f3c080163b0e
Maarij <maarij.anwar@gmail.com>
2024-12-14 10:03:19 -0600

    100. Same Tree

--- src/main/java/blind75/Trees/InvertBinaryTree.java
+++ src/main/java/blind75/Trees/InvertBinaryTree.java
@@ -36,21 +36,4 @@ public class InvertBinaryTree {
     }
 }
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
 
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
---
+++ src/main/java/blind75/Trees/SameTree.java
@@ -0,0 +1,34 @@
+package main.java.blind75.Trees;
+
+/**
+ * <a href="https://leetcode.com/problems/same-tree/description/">100. Same Tree</a>
+ * <br>
+ * <br>
+ * Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+ * Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+ */
+public class SameTree {
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+
+        node1.left = node2;
+        node1.right = node3;
+
+        System.out.println(isSameTree(node1, node1));
+    }
+
+    // TC: o(p+q)
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p != null && q != null && p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        } else {
+            return false;
+        }
+    }
+}
